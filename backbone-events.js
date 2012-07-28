5bd93b9ceffcbbad4824f4f650a1a8c7015cba33
Ian Storm Taylor <ian@ianstormtaylor.com>
2012-07-28 14:42:03 -0700

    cleaned up a lot of logic

--- backbone-events.js
+++ backbone-events.js
@@ -9,59 +9,79 @@
   Backbone.mixin || (Backbone.mixin = {});
   Backbone.mixin.events = function () {
 
-    // Delegates all Backbone events as passed in or as defined in `this.backboneEvents`.
+    // A regex to split commas with optional spaces around them.
+    var commaSplitter = /\s*,\s*/;
+    // A regex to split dot-separated, properties.
+    var propertySplitter = /\./g;
+
+    // Delegates all Backbone events as passed in or as defined in
+    // `this.backboneEvents`.
     this.prototype.delegateBackboneEvents = function (events) {
       events || (events = this.backboneEvents) || (events = {});
       this._backboneEvents || (this._backboneEvents = []);
       this.undelegateBackboneEvents();
 
       for (var key in events) {
-        // A regex to split commas with optional spaces around them.
-        var commaSplitter = /\s*,\s*/;
-        // A regex to split optional, dot-separated, properties.
-        var propertySplitter = /(\w+)(\.(\w+))?/;
-        var values, event, method,
-            keys = key.split(commaSplitter),
-            // Default subject and context to this.
-            subject = this,
-            context = this;
+        var keys   = key.split(commaSplitter);
+        var values = typeof events[key] === 'string' ? [events[key]] : events[key];
 
+
+        // Event
+        // -----
+
+        var event = keys[keys.length-1];
+        if (event === undefined) throw new Error('Invalid event: ' + key);
+
+
+        // Subject
+        // -------
+
+        var subject = this;
         // If we have a subject other than this...
         if (keys.length === 2) {
           // Split subject from sub-subjects.
-          var subjs = propertySplitter.exec(keys[0]);
-          subject = this[subjs[1]];
-          // Special case to let models listen to their attributes. Useful only for view models, otherwise its reaching.
-          if (subject instanceof Backbone.Model && subjs[3])
-            subject = subject.attributes[subjs[3]];
+          var subjects = keys[0].split(propertySplitter);
+          subject = this[subjects[0]];
+          // Recursively travel down properties, and allow for the properties
+          // actually being inside a Model's `attributes`.
+          for (var l = 1, subsubject; subsubject = subjects[l]; l++) {
+            subject = subject instanceof Backbone.Model ? subject.get(subsubject) : subject[subsubject];
+          }
         }
 
-        // The event is either the only or second value, and we need one.
-        event = keys.length === 1 ? keys[0] : keys[1];
-        if (!event) throw new Error('Invalid event');
-
-        // Iterate over values, binding each one to the subject + event. Make sure value is an array, so we can always just iterate.
-        values = typeof events[key] === 'string' ? [events[key]] : events[key];
+        // Iterate over values, binding each one to the subject + event.
         for (var i = 0, value; value = values[i]; i++) {
           value = value.split(commaSplitter);
-          // Split method-subject from method.
-          var meths = propertySplitter.exec(value[0]);
 
+
+          // Method
+          // ------
+
+          var methods = value[0].split(propertySplitter);
           // Either a method on a subview or a method on this.
-          if (this[meths[1]]) method = meths[3] ? this[meths[1]][meths[3]] : this[meths[1]];
-          // No event found.
-          if ((this[meths[1]] && meths[3] &&
-               this[meths[1]][meths[3]] === undefined) ||
-              (this[meths[1]] === undefined &&
-               meths[3] === undefined))
-            throw new Error('Cant find method');
+          var method = methods[1] ? this[methods[0]][methods[1]] : this[methods[0]];
+          if (method === undefined) throw new Error('Cant find method: ' + value[0]);
 
-          // If a context is passed in use it, otherwise if the method has a subject other than this use it's subject, otherwise use this.
+
+          // Context
+          // -------
+
+          var context = this;
+          // If a context is passed in use it, otherwise if the method has a
+          // subject other than this use it's subject, otherwise use this.
           if (value[1] && this[value[1]] && value[1] !== 'this') context = this[value[1]];
-          else if (meths[3] && this[meths[1]]) context = this[meths[1]];
+          else if (methods[1] && this[methods[0]]) context = this[methods[0]];
 
-          // Bind to each event in the list. Subjects and method/context subjects always fail silently so that events can be added to the dictionary even if the subject only gets created in certain cases. Otherwise, those cases would always require breaking out of the dictionary and then is isn't as useful because it doesn't hold all the events.
-          if (subject && method && context) {
+
+          // Bind
+          // ----
+
+          // Bind to each event in the list. Subjects always fail silently so
+          // that events can be added to the dictionary even if the subject only
+          // gets created in certain cases. Otherwise, those cases would always
+          // require breaking out of the dictionary and then it isn't as useful
+          // because it doesn't hold all your events nicely.
+          if (subject) {
             subject.on(event, method, context);
             this._backboneEvents.push([subject, event, method, context]);
           }
